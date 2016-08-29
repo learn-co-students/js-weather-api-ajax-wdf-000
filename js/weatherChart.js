9c1a3e98b44c5b154bb7cf50c6bc3612820c4e15
thecarsgone <carolina.nicole.gonzalez@gmail.com>
2016-08-29 11:16:26 -0400

    Done

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,38 @@
 function getFarenheits(result){
-  // Your code goes here
+  fahrenheits = result.hourly_forecast.map(function(item) {
+    return item.temp.english;
+  });
+  return fahrenheits;
 }
 
 function getHours(result){
-  // Your code goes here
+  hours = result.hourly_forecast.map(function(item) {
+    return item.FCTTIME.hour;
+  });
+  return hours;
 }
 
 function generateDataSet(labels, data) {
-  // Your code goes here
+  result = {}
+  result['labels'] = labels;
+  result['datasets'] = [{
+    label: "Hourly Weather for New York",
+    fillColor: "rgba(220,220,220,0.2)",
+    strokeColor: "rgba(220,220,220,1)",
+    pointColor: "rgba(220,220,220,1)",
+    pointStrokeColor: "#fff",
+    pointHighlightFill: "#fff",
+    pointHighlightStroke: "rgba(220,220,220,1)",
+    data: ["data1", "data2"]
+  }];
+  result['datasets'][0]['data'] = data;
+  return result;
 }
 
 function makeAjaxRequest(endpoint, success) {
-  // Your code goes here
+  $.ajax({
+  url: endpoint,
+  dataType: 'jsonp',
+  success: success
+});
 }
