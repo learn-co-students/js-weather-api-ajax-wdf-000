897b626eeb79ae89214f45aa28f6c4405dc9c30d
satub <satu.barnhill@gmail.com>
2016-10-06 14:56:36 -0400

    Tests pass too

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -3,7 +3,10 @@ function getFahrenheits(result){
   // test:  console.log(result.hourly_forecast[0].temp.english);
   ///result.hourly_forecast : this is an array of weather objects for each hour
   return $.map(result.hourly_forecast, function(hourly, index){
-    return parseInt(hourly.temp.english);
+    // return parseInt(hourly.temp.english);
+    /// You can't use parseInt if you want your tests to pass.
+    ///I mean, it makes no sense what-so-ever in Javascript to make sure that your numbers are numbers...right?right?
+    return hourly.temp.english;
   });
 
 }
@@ -11,7 +14,8 @@ function getFahrenheits(result){
 function getHours(result){
   // test:  console.log(result.hourly_forecast[0]["FCTTIME"].hour);
   return $.map(result.hourly_forecast, function(hourly, index){
-    return parseInt(hourly["FCTTIME"].hour);
+    // return parseInt(hourly["FCTTIME"].hour);
+    return hourly["FCTTIME"].hour;  ///the spec wants back an array of strings, so don't return numbers
   });
 }
 
@@ -20,35 +24,24 @@ function generateDataSet(labels, data) {
    labels: labels,
     datasets: [
         {
-            label: "NYC Hourly Temperature for today",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
+            label: "Hourly Weather for New York",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
             data: data,
-            spanGaps: false,
         }
     ]
   };
 }
 
+
 function makeAjaxRequest(endpoint, success) {
   $.ajax({
     url: endpoint,
-    type: 'GET',
+    // type: 'GET',  /// can't specify the request type if you want to pass the test;
     dataType: 'jsonp',
     success: success
   });
