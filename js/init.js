826d435233cb1d2f005264eb6dd5bc9fef5003d0
fidelscodes <fidelscodes@gmail.com>
2016-10-25 11:13:04 -0400

    Done

--- js/init.js
+++ js/init.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function($) {
-  var API_KEY = "GO GET AN API KEY";
+  // var API_KEY = "ENTER YOUR API KEY HERE";
+  var API_KEY = "bd4b048cb906f69e";
   var URL = "http://api.wunderground.com/api/" + API_KEY + "/hourly/q/NY/New_York.json";
   var ctx = $("#NYCWeatherChart").get(0).getContext("2d");
 
