f709d337d54cde10a249c73ef5dd98feb6c96b94
sami4thewin <samiomarmohammed@gmail.com>
2016-11-11 03:44:03 -0500

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,44 @@
 function getFahrenheits(result){
   // Your code goes here
+  return result.hourly_forecast.map(function(forecast, index) {
+    var temp = forecast.temp;
+    return temp.english;
+  });
 }
 
 function getHours(result){
   // Your code goes here
+  return result.hourly_forecast.map(function(forecast, index) {
+    var time = forecast.FCTTIME;
+    return time.hour;
+  });
 }
 
 function generateDataSet(labels, data) {
   // Your code goes here
+  var newData = {
+    labels: labels,
+    datasets: [
+      {
+        label: "Hourly Weather for New York",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: data
+      }
+    ]
+  };
+  return newData;
 }
 
 function makeAjaxRequest(endpoint, success) {
   // Your code goes here
+  $.ajax({
+    url: endpoint,
+    dataType: 'jsonp',
+    success: success
+  });
 }
