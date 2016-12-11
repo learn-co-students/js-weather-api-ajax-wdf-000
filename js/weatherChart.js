1ca41f70f415806c22e62898d1d86f0ba7c81ae3
Dmitriy Khait <khaitdmitriy@gmail.com>
2016-12-10 23:55:59 -0500

    Done.

--- js/weatherChart.js
+++ js/weatherChart.js
@@ -1,15 +1,48 @@
 function getFahrenheits(result){
+  return result.hourly_forecast.map(function(hourly){
+    return hourly.temp.english;
+  })
   // Your code goes here
 }
 
 function getHours(result){
-  // Your code goes here
+   // Your code goes here
+   return result.hourly_forecast.map(function(hourly) {
+    return hourly.FCTTIME.hour;
+  });
 }
 
 function generateDataSet(labels, data) {
   // Your code goes here
+  var cdata = {
+    labels: labels,
+    datasets: [
+        {
+            label: "Hourly Weather for New York",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(220,220,220,1)",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: data
+        }
+    ]
+};
+
+  return cdata;
+
 }
 
 function makeAjaxRequest(endpoint, success) {
   // Your code goes here
+  // console.log(success)
+   $.ajax({
+     url: endpoint,
+     dataType : "jsonp",
+     success: success
+   });
+
+  //  $.ajax({ url : endpoint, dataType : "jsonp", success: success})
+
 }
